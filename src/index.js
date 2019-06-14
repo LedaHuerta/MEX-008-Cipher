dfa8b2bf57f6105e756ecaaeba48462f11580c88
LedaHuerta <ledamhuerta@gmail.com>
2019-06-14 02:35:47 -0500

    agrgando boton retorno a fake, modificaciones de estilo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //Las variables llaman a los elementos en el HTML de la sección fake: button//
 const buttonMetamorfosis = document.getElementById ('button-metamorfosis');
+const buttonMetamorfosisBack = document.getElementById ('button-metamorfosis-back');
 //variable para mostrar segunda sección//
 const showCipherSection = document.getElementById('cipher-caesar');
 //variable para ocultar la primera sección//
@@ -10,6 +11,12 @@ const metamorfosis = () => {
     hideFakeSection.classList.add('hide');
     showCipherSection.classList.remove('hide');
 }
+//Crear una función flecha que manipule las secciones de vuelta//
+const metamorfosisBack = () => {
+    hideFakeSection.classList.remove('hide');
+    showCipherSection.classList.add('hide');
+}
 
 //Crear un evento que desencadene la función//
 buttonMetamorfosis.addEventListener('click',metamorfosis);
+buttonMetamorfosisBack.addEventListener('click',metamorfosisBack);
